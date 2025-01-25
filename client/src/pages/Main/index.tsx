8d9cf38e45e48ad730e05ad656db5553757c31a3
Vladimir Strashko <haywaster3@gmail.com>
2025-01-25 18:22:15 +0700

    Fix: Job recovery husky & lint-staged

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -17,7 +17,7 @@ export const Main: FC = () => {
   return (
     <>
       <Header />
-      <Container as='main' direction='column'>
+      <Container as="main" direction="column">
         <Title>Hello! Write your name and chose the game</Title>
         <ChooseGame />
       </Container>
